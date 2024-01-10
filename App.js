10ad922e80a2ce57b3b9b5bc85ee7c914e7bf42a
Franso <69063412+Franso06@users.noreply.github.com>
2024-01-10 00:02:20 -0300

    checkpoint
    avance login

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Merriweather-Italic': require('./assets/fonts/Merriweather-Italic.ttf'),
+  });
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {fontsLoaded && <Text style={{fontFamily:'Merriweather-Italic', fontSize:30, padding:5}}>MiAula </Text>}
+      <TextInput placeholder= 'Correo' style={styles.input}></TextInput>
+      <TextInput placeholder= 'Contraseña'style={styles.input}></TextInput>
+      <Text>Olvide mi contraseña </Text>
+      <TouchableOpacity style= {styles.boton} title='Ingresar'>
+        <Text>Ingresar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +33,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 30,
+    width: 150,
+    margin: 12,
+    borderWidth: 0.5,
+    borderRadius: 5,
+    padding: 5,
+  },
+  boton: {
+    borderRadius: 10,
+    padding: 8,
+    backgroundColor: '#efefef',
+  },
 });
+
+
