f25312daf4cba150ff5b0a1f5986973dcbf55424
Franso <69063412+Franso06@users.noreply.github.com>
2024-01-30 18:32:49 -0300

    login y header
    vista principal lista

--- screens/Login.js
+++ screens/Login.js
@@ -1,41 +1,76 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Button, Avatar } from '@rneui/base';
+import { useNavigation } from '@react-navigation/native';
+import {LinearGradient} from 'expo-linear-gradient';
+import MaskedView from "@react-native-masked-view/masked-view";
 import React from 'react';
 
-const Login = () => {
+export const Login = () => {
+    const navigation = useNavigation();
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
-  
     const [fontsLoaded] = useFonts({
       'Merriweather-Italic': require('../assets/fonts/Merriweather-Italic.ttf'),
+      'DancingScript-Regular': require('../assets/fonts/DancingScript-Regular.ttf'),
+      'Kalam-Light': require('../assets/fonts/Kalam-Light.ttf'),
+      'AmaticSC-Bold': require('../assets/fonts/AmaticSC-Bold.ttf'),
+      'AmaticSC-Regular': require('../assets/fonts/AmaticSC-Regular.ttf'),
+
     });
+      const GradientText = (props) => {
+      return (
+        <MaskedView maskElement={<Text {...props} />}>
+          <LinearGradient
+            colors={["#df54f2","#5986ff","#00ffe7"]}
+            start={{ x: 0, y: 0.5 }}
+            end={{ x: 1, y: 0.5 }}
+          >
+            <Text {...props} style={[props.style, { opacity: 0 }]} />
+          </LinearGradient>
+        </MaskedView>
+      );
+    };
+    
     const Formulario = () => {
       return <>
           <View style={styles.container}>
-            {fontsLoaded && <Text style={{fontFamily:'Merriweather-Italic', fontSize:25, marginTop:150, marginBottom:30,marginLeft:15, alignSelf:'flex-start'}}>Ingresa o registrate </Text>}
+            
+              <View style={{marginBottom:50}}></View>
+              {fontsLoaded &&<GradientText style={styles.titulo}>MiAula</GradientText>}
+              <Text style={{fontSize: 12.5,alignSelf:'flex-end', color: "#7D7C7C",right:30,top:2}}>para alumnos y profesores </Text>
+
+              <Image style={styles.imagen} source={require('../assets/logo.png')} />
+            
+            <Text 
+              style={styles.subtitulo}
+              >Ingresa o registrate </Text>
   
             <TextInput 
-            placeholder= 'Correo' 
-            style={styles.input}
-            type="email"
-            // onChangeText={(text)=> setEmail(text)}
+                placeholder= 'Correo' 
+                style={styles.input}
+                type="email"
             ></TextInput>
   
             <TextInput
-            placeholder= 'Contraseña'
-            style={styles.input}
-            secureTextEntry={true}
-            // onChangeText={(text)=> setEmail(text)}
+                placeholder= 'Contraseña'
+                style={styles.input}
+                secureTextEntry={true}
             ></TextInput>
             <Text 
-            style={styles.olvideContrasenia}>
-              No tienes cuenta? <Text style={{textDecorationLine: 'underline', color:'black'}}>Registrate</Text>  </Text>
+                style={styles.olvideContrasenia}>
+                No tienes cuenta? <Text onPress={() => navigation.navigate('Register') }style={{textDecorationLine: 'underline', color:'black'}}>Registrate</Text>  </Text>
             <Button  type="outline" 
-              title="Ingresar"
-              titleStyle={{color:'white'}} 
-              buttonStyle={{backgroundColor: 'black', width:'95%',marginBottom:7, height:45, alignSelf:'center', marginTop:45, borderColor:'black', borderRadius:5}}></Button>
-            
+                title="Ingresar"
+                titleStyle={{color:'white'}} 
+                ViewComponent={LinearGradient}
+                linearGradientProps={{
+                    colors:["#df54f2","#5986ff","#00ffe7"],
+                    start: { x: 0, y: 0.5 },
+                    end: { x: 1, y: 0.5 },
+                }}
+                buttonStyle={{ width:'95%',marginBottom:7, height:45, alignSelf:'center', marginTop:20, borderRadius:5}}></Button>
+
             <Text style={{alignSelf:'center'}}>o</Text>
   
             <Button 
@@ -61,7 +96,27 @@ const Login = () => {
     );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
+    imagen:{
+      width:180,
+      height:180,
+      marginBottom:-90,
+      bottom:110,
+      marginLeft:-30,
+    },
+    titulo:{
+      fontSize: 80,
+      fontFamily: 'AmaticSC-Bold', 
+      marginLeft:155,
+      margin:-15,
+    },
+    subtitulo:{
+        fontSize:20, 
+        marginTop:0, 
+        marginBottom:10,
+        marginLeft:10, 
+        alignSelf:'flex-start'
+    },
     container: {
       flex: 1,
       backgroundColor: '#F1EFEF',
@@ -83,10 +138,7 @@ const styles = StyleSheet.create({
       margin: 8,
       borderRadius: 5,
       marginTop: 40,
-      backgroundColor: '#191717',
-      
     },
-   
     text: {
       fontSize: 16,
       lineHeight: 21,
