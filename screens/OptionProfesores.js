53000a4c6ffeba3fe52e2c3d7f1f1217a9bcf315
Franso <69063412+Franso06@users.noreply.github.com>
2024-02-23 13:46:11 -0300

    avance
    asdl

--- screens/OptionProfesores.js
+++ screens/OptionProfesores.js
@@ -1,15 +1,43 @@
-import { View, Text, TextInput } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput } from 'react-native';
+import React, {useState} from 'react';
 import { styles } from "../components/styles";
 import { Button } from "@rneui/base";
 import { LinearGradient } from "expo-linear-gradient";
 import { useNavigation } from "@react-navigation/native";
+import { firebase } from "../firebase-config";
+import QRCode from 'react-native-qrcode-svg';
 import Header from "./Header";
 
 const OptionProfesores = () => {
     const navigation = useNavigation();
+    const profesor = firebase.auth().currentUser.uid;
+    if (profesor){
+      console.log(profesor);
+    };
 
+    const [curso, setCurso] = useState(null);
+    const [asignatura, setAsignatura] = useState(null);
 
+    const ingresarDatos = async () => {
+    try {
+      await firebase
+        .firestore()
+        .collection("Clase")
+        .add({
+          profesor: {profesor},
+          curso: { curso },
+          asignatura: { asignatura },
+        })
+        .then(() => {
+          console.log("Clase agregada!");
+          navigation.navigate('Home');
+        });
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+ 
     //ToDo: Que guarde la informacion en la bd y ademas crear un QR con el id de la clase
 
   return (
@@ -19,13 +47,17 @@ const OptionProfesores = () => {
       <Text style={styles.subtitulo}>Creación de la clase </Text>
 
       <Text style={styles.olvideContrasenia}>Curso:</Text>
-      <TextInput style={styles.input}/>
+      <TextInput
+      onChangeText={value => {setCurso(value); console.log("curso: "+value);}} 
+      style={styles.input}/>
       
       <Text style={styles.olvideContrasenia}>Asignatura:</Text>
-      <TextInput style={styles.input}/>
+      <TextInput 
+      onChangeText={value => {setAsignatura(value); console.log("asignatura: "+value);}}       
+      style={styles.input}/>
       
       <Button 
-      onPress={() => navigation.navigate('Home')}
+      onPress={() => ingresarDatos()}
       ViewComponent={LinearGradient}
       linearGradientProps={{
         colors: ["#fd3cff","#00cdc8"],
@@ -35,6 +67,12 @@ const OptionProfesores = () => {
       buttonStyle={styles.button}
       >Crear
       </Button>
+      {/* <QRCode
+        value={profesor}
+        size={200}
+        color='white'
+        backgroundColor='black'>
+        </QRCode> */}
     </View>
   )
 }
